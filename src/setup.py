a63b0be51e698999bce9ced2087e9269032e3c36
Kim Fisher <kfisher@wcs.org>
2019-10-28 11:28:59 -0400

    turn task_base into package

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="scl-task_base",
+    description="Base python classes for creating HII and SCL tasks",
+    version="0.1",
+    py_modules=["task_base"],
+    install_requires=["earthengine-api"],
+)
