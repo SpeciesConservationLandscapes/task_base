1f0a7da6122d39fcdc96ad7f8f9a32fcbd99f418
Kim Fisher <kfisher@wcs.org>
2019-10-31 16:22:53 -0400

    initial base classes refactoring

--- task_base.py
+++ task_base.py
@@ -1,6 +1,7 @@
 import os
 import json
 import ee
+import time
 from datetime import datetime, timezone
 
 
@@ -8,19 +9,13 @@ class Task(object):
     DATE_FORMAT = '%Y-%m-%d'
     # list of possible statuses associated with a Task instance -- which is different from ee task statuses
     NOTSTARTED = "not started"
-    SKIPPED = "skipped"
+    FAILED = "failed"
     RUNNING = "running"
     COMPLETE = "complete"
 
     status = NOTSTARTED
     inputs = {}
-    crs = 'EPSG:4326'
-    scale = 300
-    aoi = [[[[-180.0, -90.0],
-             [180.0, -90.0],
-             [180.0, 90.0],
-             [-180.0, 90.0],
-             [-180.0, -90.0]]]]
+    wait_for_outputs = True
 
     def __init__(self, *args, **kwargs):
         _taskdate = datetime.now(timezone.utc).date()
@@ -30,21 +25,68 @@ class Task(object):
             pass
         self.taskdate = _taskdate
 
-    def check_inputs(self):
-        if (not hasattr(self, 'aoi') or not hasattr(self, 'scale') or not hasattr(self, 'crs') or
-                not self.aoi or not self.scale or not self.crs):
-            raise NotImplementedError('Undefined input: aoi, scale, or crs')
+        self.wait_for_outputs = kwargs.pop('wait_for_outputs', True)
 
-    def run(self):
+    def check_inputs(self):
+        pass
+
+    def run_calc(self):
+        raise NotImplementedError('`run_calc` must be defined')
+
+    def run(self, **kwargs):
+        wait_function = kwargs.pop('wait_function', None)
         self.status = self.RUNNING
         self.check_inputs()
+        try:
+            self.run_calc()
+            if wait_function:
+                wait_function()
+            self.status = self.COMPLETE
+            print('status: {}'.format(self.status))
+        except Exception as e:
+            self.status = self.FAILED
+            raise type(e)(str(e)) from e
 
 
-class EETask(Task):
+class GeoTask(Task):
+    crs = 'EPSG:4326'
+    scale = 300
+    aoi = [[[[-180.0, -90.0],
+             [180.0, -90.0],
+             [180.0, 90.0],
+             [-180.0, 90.0],
+             [-180.0, -90.0]]]]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def check_inputs(self):
+        super().check_inputs()
+        if (not hasattr(self, 'aoi') or not hasattr(self, 'scale') or not hasattr(self, 'crs') or
+                not self.aoi or not self.scale or not self.crs):
+            self.status = self.FAILED
+            raise NotImplementedError('Undefined input: aoi, scale, or crs')
+
+    def run_calc(self):
+        super().run_calc()
+
+    def run(self, **kwargs):
+        super().run(**kwargs)
+
+
+class EETask(GeoTask):
     service_account_key = os.environ.get('SERVICE_ACCOUNT_KEY')
     ee_rootdir = None
-    ee_taskids = []
+    ee_tasks = {}
     ee_max_pixels = 500000000000
+
+    EEREADY = 'READY'
+    EE = 'RUNNING'
+    EECOMPLETED = 'COMPLETED'
+    EEFAILED = 'FAILED'
+    EECANCELLED = 'CANCELLED'
+    EEUNKNOWN = 'UNKNOWN'
+    EEFINISHED = [EECOMPLETED, EEFAILED, EECANCELLED, EEUNKNOWN]
 
     @staticmethod
     def _create_ee_path(asset_path):
@@ -76,10 +118,11 @@ class EETask(Task):
             ee.Initialize(credentials)
 
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        super().__init__(*args, **kwargs)
         self._initialize_ee_client()
 
         if not self.ee_rootdir:
+            self.status = self.FAILED
             raise NotImplementedError('`ee_rootdir` must be defined')
         self.ee_rootdir = self.ee_rootdir.strip('/')
         self._create_ee_path(self.ee_rootdir)
@@ -93,31 +136,33 @@ class EETask(Task):
     def check_inputs(self):
         super().check_inputs()
 
-        # TODO: test aoi validity outside ee and move this into parent
+        # TODO: test aoi validity outside ee and move this into GeoTask?
         try:
             ee_aoi = ee.Geometry.Polygon(coords=self.aoi)
         except Exception as e:
+            self.status = self.FAILED
             raise type(e)(str(e) + ' `aoi` incorrect: {}'.format(self.aoi)) from e
 
         for key, ee_input in self.inputs.items():
-            if 'ee_path' in ee_input:
-                if not ee.data.getInfo(ee_input['ee_path']):
-                    self.status = self.SKIPPED
-                    print('{} does not exist; task skipped'.format(ee_input['ee_path']))
-                    break
+            if 'ee_path' not in ee_input:
+                continue
 
-                ic = ee.ImageCollection(ee_input['ee_path'])
-                ee_taskdate = ee.Date(self.taskdate.strftime(self.DATE_FORMAT))
-                most_recent = ic.filterDate('1900-01-01', ee_taskdate)\
-                    .sort('system:time_start', False)\
-                    .first()
-                most_recent_date = ee.Date(most_recent.get("system:time_start"))
-                age = ee_taskdate.difference(most_recent_date, 'year').getInfo()
-                if age > ee_input['maxage']:
-                    self.status = self.SKIPPED
-                    print('{} most recent image is {} years old (maxage: {}); task skipped'.format(
-                        ee_input['ee_path'], age, ee_input['maxage']))
-                    break
+            if not ee.data.getInfo(ee_input['ee_path']):
+                print('{} does not exist'.format(ee_input['ee_path']))
+                break
+
+            ic = ee.ImageCollection(ee_input['ee_path'])
+            ee_taskdate = ee.Date(self.taskdate.strftime(self.DATE_FORMAT))
+            most_recent = ic.filterDate('1900-01-01', ee_taskdate)\
+                .sort('system:time_start', False)\
+                .first()
+            most_recent_date = ee.Date(most_recent.get("system:time_start"))
+            age = ee_taskdate.difference(most_recent_date, 'year').getInfo()
+            if age > ee_input['maxage']:
+                self.status = self.FAILED
+                print('{} most recent image is {} years old (maxage: {})'.format(
+                    ee_input['ee_path'], age, ee_input['maxage']))
+                break
 
     # ee asset property values must currently be numbers or strings
     def flatten_inputs(self):
@@ -147,24 +192,35 @@ class EETask(Task):
             maxPixels=self.ee_max_pixels,
         )
         image_export.start()
-        self.ee_taskids.append(image_export.id)
+        self.ee_tasks[image_export.id] = {}
 
-    def run(self):
-        super().run()
+    def run(self, **kwargs):
+        super().run(**dict(kwargs, wait_function=self.wait_for_ee))
 
-    def get_unfinished_ee_tasks(self):
-        unfinished_tasks = []
-        if self.ee_taskids:
+    def wait_for_ee(self):
+        max_sleep = 600
+        counter = 3
+        while self.ee_tasks:
+            self.update_ee_tasks()
+            if not self.ee_tasks:
+                break
+            counter += 1
+            sleeptime = 2 ** counter
+            if sleeptime > max_sleep:
+                sleeptime = max_sleep
+            time.sleep(sleeptime)
+
+    def update_ee_tasks(self):
+        if self.ee_tasks:
             # possible ee task states: READY, RUNNING, COMPLETED, FAILED, CANCELLED, UNKNOWN
-            statuses = ee.data.getTaskStatus(self.ee_taskids)
+            statuses = ee.data.getTaskStatus(self.ee_tasks.keys())
             print(statuses)
             for s in statuses:
-                if s['state'] in ['READY', 'RUNNING']:
-                    unfinished_tasks.append(s['id'])
-                elif s['state'] == 'UNKOWN':
-                    raise ValueError('unknown task {}'.format(s['id']))
-
-        return unfinished_tasks
+                if s['state'] in self.EEFINISHED:
+                    del self.ee_tasks[s['id']]
+                    # TODO: log and update self.status based on EECOMPLETED, EEFAILED, EECANCELLED, EEUNKNOWN
+                else:
+                    self.ee_tasks[s['id']] = s
 
 
 class SCLTask(EETask):
@@ -172,7 +228,7 @@ class SCLTask(EETask):
     ee_aoi = 'aoi'
 
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        super().__init__(*args, **kwargs)
         try:
             self.species = kwargs.pop('species', None)
         except (TypeError, ValueError):
