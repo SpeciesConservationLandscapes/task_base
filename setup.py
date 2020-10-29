7bba2771126c6f478977cf091d44f9178335d903
Kim Fisher <kfisher@wcs.org>
2020-10-29 16:17:41 -0400

    tweaks and fixes
    add return id from ee save methods
    make extent Geometry/list of coordinates
    make pyramiding optional arg
    make clean_up method hook
    fix sha properties write

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="scl-task_base",
     description="Base python classes for creating HII and SCL tasks",
-    version="0.60",
+    version="0.70",
     py_modules=["task_base"],
     install_requires=["earthengine-api"],
 )
