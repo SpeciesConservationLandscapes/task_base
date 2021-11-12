d5cf4bb431220878dffdff72f5171f361032d123
Kim Fisher <kfisher@wcs.org>
2021-11-12 12:45:08 -0500

    update setup requirements, bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="scl-task_base",
     description="Base python classes for creating HII and SCL tasks",
-    version="0.82",
+    version="0.90",
     py_modules=["task_base"],
-    install_requires=["earthengine-api"],
+    install_requires=["earthengine-api==0.1.254", "gitpython==3.1.14", "google-api-python-client==1.12.5"],
 )
