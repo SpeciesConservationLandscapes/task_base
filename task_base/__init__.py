fda846b56a6648b8a40aedf67b6d42de6f34d504
Kim Fisher <kfisher@wcs.org>
2021-11-13 11:27:32 -0500

    expose EETaskError

--- task_base/__init__.py
+++ task_base/__init__.py
@@ -1,6 +1,6 @@
 from .task import Task
 from .geotask import GeoTask
-from .eetask import EETask
+from .eetask import EETask, EETaskError
 from .hiitask import HIITask
 from .scltask import SCLTask
 from .data_transfer import ConversionException
