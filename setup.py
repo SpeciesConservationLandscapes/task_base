6e46530824b3cb586efe4d54dc8692c20a77d00a
Kim Fisher <kfisher@wcs.org>
2021-11-13 06:52:35 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="scl-task_base",
-    description="Base python classes for creating HII and SCL tasks",
-    version="1.0",
-    py_modules=["task_base"],
+    version="1.0.0",
+    packages=["task_base"],
     install_requires=[
         "earthengine-api==0.1.254",
         "gitpython==3.1.14",
         "google-api-python-client==1.12.5",
     ],
+    description="Base python classes for creating HII and SCL tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
