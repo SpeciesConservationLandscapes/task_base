4ede93b19cebbf3088303fce0df744225092e284
Kim Fisher <kfisher@wcs.org>
2022-01-24 14:49:56 -0500

    hotfix: export PROJECTS

--- task_base/__init__.py
+++ task_base/__init__.py
@@ -1,6 +1,6 @@
 from .task import Task
 from .geotask import GeoTask
-from .eetask import EETask, EETaskError
+from .eetask import EETask, EETaskError, PROJECTS
 from .hiitask import HIITask
 from .scltask import SCLTask
 from .data_transfer import ConversionException
